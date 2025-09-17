6f4c53d7911f769cc4b8d321678aa734f85c86ab
fjkk <fujikake.takayoshi@gmail.com>
2025-09-17 20:33:52 +0900

    配列の中の値を直接上書き

--- array_control.py
+++ array_control.py
@@ -1,8 +1,8 @@
 lst = [1, 2, 3, 4]
-for i in lst:
-    i *= 2
+
+lst2 = [x * 2 for x in lst]
+print(lst2)
+
+for i in range(len(lst)):
+    lst[i] *= 2
 print(lst)
-
-
-lst3 = [x * 2 for x in lst]
-print(lst3)  # [2, 4, 6, 8]
