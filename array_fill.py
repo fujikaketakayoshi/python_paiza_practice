e49ed07236708d36d86a0c36d2cbee6a1f167f10
fjkk <fujikake.takayoshi@gmail.com>
2025-09-18 17:21:06 +0900

    array_fill追加

---
+++ array_fill.py
@@ -0,0 +1,4 @@
+h = 2
+w = 4
+arr = [[0]*w for _ in range(h)]
+print(arr)
--- array_join.py
+++ array_join.py
@@ -2,3 +2,6 @@ lst = ['a', 'b', 'c', 'd']
 print('-'.join(lst))  # a-b-c-d
 print(''.join(lst))   # abcd
 print(', '.join(lst)) # a, b, c, d
+
+arr = [1, 2, 3, 4]
+print("\n".join(arr))
